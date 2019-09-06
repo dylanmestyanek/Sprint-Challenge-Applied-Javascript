cb1e35054af487c82b5aa14fef283be8d3998317
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-06 10:52:55 -0500

    Tabs/index.js complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(respo => {
+        obj = respo.data.articles;
+        for (let article in obj){
+            obj[article].forEach(article => cardsContainer.appendChild(createCard(article.authorName, article.authorPhoto, article.headline)));
+        }
+    })
+    .catch(err => console.log('Failed to grab data', err));
+
+
+function createCard(name, img, headline){
+    const card = document.createElement('div');
+    const headlineContainer = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const authorImg = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.classList.add('card');
+    headlineContainer.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+    
+    headlineContainer.textContent = headline;
+    authorName.textContent = name;
+    authorImg.src = img;
+
+    card.appendChild(headlineContainer);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(authorImg);
+    author.appendChild(authorName);
+
+    return card;
+};
